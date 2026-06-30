f1801de5c19eea79c26082ba168c2938191786f8
subham-28 <subhamomm836@gmail.com>
2026-06-30 20:53:36 +0530

    Upgraded Chatbot

--- ChatBot/langgraph_database_backend.py
+++ ChatBot/langgraph_database_backend.py
@@ -2,9 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from dotenv import load_dotenv
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langchain_openrouter import ChatOpenRouter
+import sqlite3
 
 
 load_dotenv()
@@ -27,8 +28,10 @@ def chat_node(state: ChatState):
     response=llm.invoke(query)
     return {'messages': [response]}
 
+conn=sqlite3.connect(database='chatbot.db',check_same_thread=False)
 
-checkpointer=InMemorySaver()
+
+checkpointer=SqliteSaver(conn=conn)
 
 graph=StateGraph(ChatState)
 
@@ -39,9 +42,16 @@ graph.add_edge('chat_node',END)
 
 workflow=graph.compile(checkpointer=checkpointer)
 
-workflow.invoke(
-                    {'messages': [HumanMessage(content='Hi my name is Subham')]},
-                    config=CONFIG
-                )
+# #test
+# response=workflow.invoke(
+#                     {'messages': [HumanMessage(content="Hi my name is Subham")]},
+#                     config=CONFIG,
+#                 )
+# print(response)
 
-print(workflow.get_state(config=CONFIG).values['messages'])
+def retrieve_all_threads():
+    all_threads=set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
